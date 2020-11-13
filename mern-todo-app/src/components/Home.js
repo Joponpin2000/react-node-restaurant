7e600b91ef54819a21dc765e814b227127120a33
Idowu Joseph <idowuayanfeoluwa@gmail.com>
2020-11-14 00:36:33 +0100

    fixed the comment line inside return statement breaking deployed version

--- mern-todo-app/src/components/Home.js
+++ mern-todo-app/src/components/Home.js
@@ -49,7 +49,7 @@ const Home = (props) => {
                                 <Row>
                                     {
                                         (products) ? (
-                                            // Map objects like this
+
                                             products.map((product, i) =>
                                                 <Fragment key={i}>
                                                     <Col md='4'>
@@ -65,7 +65,6 @@ const Home = (props) => {
                                                                 <Card.Text>
                                                                     {product.productDesc}
                                                                 </Card.Text>
-                                                                {/* <a href="" onClick={() => props.addBasket('greyTshirt')}>Add to Cart</a> */}
                                                             </Card.Body>
                                                         </Card>
                                                     </Col>
