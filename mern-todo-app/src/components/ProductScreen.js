a5f013c88d5fc2d2fb12ce630868bc1c04038e4c
Idowu Joseph <idowuayanfeoluwa@gmail.com>
2020-11-15 23:46:23 +0100

    ..

--- mern-todo-app/src/components/ProductScreen.js
+++ mern-todo-app/src/components/ProductScreen.js
@@ -47,7 +47,7 @@ const ProductScreen = (props) => {
                                     <Fragment>
                                         <Col md='4'>
                                             <Card>
-                                                <Card.Img className="product-image" src={`${server}/${getSecondPart(product.productImage)}`} alt={product.productName} />
+                                                <Card.Img className="product-image" src={`${server}/${product.productImage}`} alt={product.productName} />
                                             </Card>
                                         </Col>
                                         <Col md='4'>
@@ -55,6 +55,7 @@ const ProductScreen = (props) => {
                                                 <Card.Body>
                                                     <Card.Title>
                                                         {product.productName}
+                                                        {product.productImage}
                                                     </Card.Title><Card.Text>
                                                         4.5 stars (8 Reviews)
                                                         </Card.Text>
